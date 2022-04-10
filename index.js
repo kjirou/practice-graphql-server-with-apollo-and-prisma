e8679cb77c22565111d9577599ed117c01416783
kjirou <sorenariblog@gmail.com>
2022-04-10 23:56:07 +0900

    「Data sourcesの設定」の前まで

--- index.js
+++ index.js
@@ -1,34 +1,61 @@
 const { ApolloServer, gql } = require('apollo-server');
+const axios = require('axios');
 
 const typeDefs = gql`
   type User {
     id: ID!
     name: String!
     email: String!
+    myPosts: [Post]
+  }
+
+  type Post {
+    id: ID!
+    title: String!
+    body: String!
+    userId: ID!
   }
 
   type Query {
     hello(name: String!): String
     users: [User]
     user(id: ID!): User
+    posts: [Post]
   }
 `
-
-const users = [
-  { id: '1', name: 'John Doe', email: 'john@test.com' },
-  { id: '2', name: 'Jane Doe', email: 'jane@example.com' },
-];
 
 const resolvers = {
   Query: {
     hello: (parent, args) => `Hello ${args.name}`,
-    users: () => users,
-    user: (parent, args) => {
-      const user = users.find((user) => user.id === args.id);
-      return user;
+    users: async () => {
+      const response = await axios.get(
+        'https://jsonplaceholder.typicode.com/users'
+      );
+      return response.data;
+    },
+    user: async (parent, args) => {
+      let response = await axios.get(
+        `https://jsonplaceholder.typicode.com/users/${args.id}`
+      );
+      return response.data;
+    },
+    posts: async () => {
+      const response = await axios.get(
+        'https://jsonplaceholder.typicode.com/posts'
+      );
+      return response.data;
     },
   },
-}
+  User: {
+    myPosts: async (parent) => {
+      const response = await axios.get(
+        'https://jsonplaceholder.typicode.com/posts'
+      );
+      const myPosts = response.data.filter((post) => post.userId == parent.id);
+      return myPosts;
+    },
+  },
+};
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
