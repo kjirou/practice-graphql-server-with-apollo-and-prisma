49cdccd0c0769fa11bf6d70ffaa58b86356ae2e0
kjirou <sorenariblog@gmail.com>
2022-04-12 22:59:16 +0900

    deleteUserを定義

--- index.js
+++ index.js
@@ -51,6 +51,7 @@ const typeDefs = gql`
   type Mutation {
     createUser(name: String!, email: String!): User
     updateUser(id: Int!, name: String!): User
+    deleteUser(id: Int!): User
   }
 `
 
@@ -91,6 +92,11 @@ const resolvers = {
         data: {
           name: args.name,
         },
+      });
+    },
+    deleteUser: (_, args) => {
+      return prisma.user.delete({
+        where: { id: args.id },
       });
     },
   },
